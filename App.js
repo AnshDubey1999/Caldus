23c402fbc9bf8846ce953782e08ca76b1e2a7b2b
ansh010599 <ansh@ualberta.ca>
2021-09-11 11:37:53 -0600

    Functional bottom tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+import HomeScreen from './src/screens/HomeScreen';
+import RandomScreen from './src/screens/RandomScreen';
+import IngredientToRecipeScreen from './src/screens/IngredientToRecipeScreen';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>Fun Title</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen}/>
+          <Tab.Screen name="Random" component={RandomScreen}/>
+          <Tab.Screen name="IngredientToRecipe" component={IngredientToRecipeScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
+
+export default App;
---
+++ src/screens/IngredientToRecipeScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, StyleSheet } from 'react-native';
+
+const IngredientToRecipeScreen = () => {
+    return ( 
+        <SafeAreaView>
+            <Text>Ingredient to Recipe Page</Text>
+        </SafeAreaView>
+     );
+}
+
+const styles = StyleSheet.create({
+
+});
+ 
+export default IngredientToRecipeScreen;
