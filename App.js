501c2410c7419618bcacbc68972e1c502ddcf2f4
osmani2 <nahmed2@ualberta.ca>
2021-09-11 12:01:25 -0600

    More subs

--- App.js
+++ App.js
@@ -31,20 +31,20 @@ export default App;
 
 const swap = {
   "butter":["margarine","olive oil","apple sauce"],
-  "cream":["fat-free half-and-half","evaporated skim milk"],
+  "cream":["fat free half-and-half","evaporated skim milk"],
   "bread crumbs":["rolled oats","crushed bran cereal"],
   "cream cheese":["fat-free cream cheese","cottage cheese"],
   "egg":["egg white"],
-  "flour":["whole-wheat flour","coconut flour","nut flour","oats"],
+  "flour":["whole wheat flour","coconut flour","nut flour","oats"],
   "ground beef":["lean ground beef","ground chicken breast","ground turker breast"],
-  "mayonnaise":["reduced-fat mayonnaise","low-fat yogurt"],
+  "mayonnaise":["reduced-fat mayonnaise","low-fat yogurt","hummus","avacado"],
   "evaporated milk":["evaporated skim milk"],
-  "milk":["fat-free milk","low-fat milk"],
-  "pasta":["whole-wheat pasta"],
+  "milk":["fat free milk","low fat milk"],
+  "pasta":["whole wheat pasta"],
   "rice":["brown rice"],
-  "sour cream":["low-fat sour cream","low-fat yogurt"],
+  "sour cream":["low fat sour cream","low fat yogurt"],
   "sugar":["cinnamon","nutmeg","vanilla","apple sauce","honey","mashed bananas","maple syrup"],
-  "salt":["herbs","spices"],
+  "salt":["garlic powder", "onion powder", "paprika", "herbs"],
   "potato":["sweet potato"],
   "bacon":["prosciutto"]
   }
