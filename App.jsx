52935114af4bd0007ca13b96915aa195522e32ec
Lefan <tan6@ualberta.ca>
2021-09-11 10:30:23 -0600

    installed expo font library and changed js to jsx
    and got some roboto fonts

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { styles } from './assets/components/GlobalStyle';
+import { GeneralThemeProvider, styles } from './assets/components/GeneralThemeProvider';
 
 export default function App() {
   return (
-    <View style={styles.center}>
-      <Text style={{color: 'white'}}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GeneralThemeProvider>
+      <View style={styles.center}>
+        <Text style={styles.h2}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GeneralThemeProvider>
   );
 }
---
+++ assets/components/GeneralThemeProvider.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Roboto_400Regular, Roboto_300Light, useFonts } from "@expo-google-fonts/roboto";
+
+const primary_400 = "#ffb60d";
+const secondary_400 = "#2b2b2b";
+
+const ThemeContext = React.createContext({ fontLoaded: false });
+
+export const GeneralThemeProvider = ({ children }) => {
+    const fontLoaded = useFonts({
+        Roboto_400Regular,
+        Roboto_300Light
+    });
+
+    return (<ThemeContext.Provider
+        value={{
+            fontLoaded: fontLoaded
+        }}
+    >{children}</ThemeContext.Provider>);
+};
+
+export const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: secondary_400,
+    },
+    text: {
+        color: "white",
+    },
+    h2: {
+        fontFamily: "Roboto_300Light",
+        color: primary_400,
+        fontSize: 20,
+    },
+});
