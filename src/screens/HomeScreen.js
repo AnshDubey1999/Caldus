67d799e5fa34b44e185e39587a00a34da34cde15
ansh010599 <ansh@ualberta.ca>
2021-09-12 09:23:20 -0600

    Making no calls for the API

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,34 +2,34 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, StyleSheet, View, StatusBar, FlatList } from 'react-native';
-import { API_HOST, API_KEY } from '../general';
+import { API_HOST, API_KEY, recipesMainList } from '../general';
 
 import RecipeBlock from '../components/RecipeBlock';
 
 const HomeScreen = () => {
 
-    const [recipes, setRecipes] = useState([]);
-    const [baseUri, setBaseUri] = useState('');
+    const recipes = recipesMainList.results;
+    const baseUri = recipesMainList.baseUri;
 
-    const fetchHealthyRecipes = async () => {
-        await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=healthy&number=1", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": API_HOST,
-                "x-rapidapi-key": API_KEY
-            }
-        }).then(response => response.json().then(res => res)).then(final => {
-          setRecipes(final.results);
-          setBaseUri(final.baseUri);
-        })
-        .catch(err => {
-            console.log("Something went wrong")
-        });
-      };
+    // const fetchHealthyRecipes = async () => {
+    //     await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=healthy&number=1", {
+    //         "method": "GET",
+    //         "headers": {
+    //             "x-rapidapi-host": API_HOST,
+    //             "x-rapidapi-key": API_KEY
+    //         }
+    //     }).then(response => response.json().then(res => res)).then(final => {
+    //       setRecipes(final.results);
+    //       setBaseUri(final.baseUri);
+    //     })
+    //     .catch(err => {
+    //         console.log("Something went wrong")
+    //     });
+    //   };
 
-    useEffect(() => {
-        fetchHealthyRecipes();
-    }, []);
+    // useEffect(() => {
+    //     fetchHealthyRecipes();
+    // }, []);
 
     const renderItem = ({ item }) => {
         return(
